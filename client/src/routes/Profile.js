024ef8ef748ea68adad5c73bbd9a0aaed47be03a
Ellipog <45790498+Ellipog@users.noreply.github.com>
2023-03-21 14:45:09 +0100

    More work on login

--- client/src/routes/Profile.js
+++ client/src/routes/Profile.js
@@ -2,19 +2,31 @@ import styles from "../css/Profile.module.css";
 import { useState } from "react";
 
 function Profile() {
-	const [login, setLogin] = useState(false);
+	const [login, setLogin] = useState("false");
 
-	if (login === false) {
+	if (login === "false") {
 		return (
 			<div className={styles.container}>
-				<button onClick={() => setLogin(true)}>Logg inn</button>
-				<button>Registrer</button>
+				<button onClick={() => setLogin("login")}>Logg inn</button>
+				<button onClick={() => setLogin("register")}>Registrer</button>
 			</div>
 		);
-	} else {
+	} else if (login === "true") {
 		return (
 			<div className={styles.container}>
 				<h1>Profile</h1>
+			</div>
+		);
+	} else if (login === "login") {
+		return (
+			<div className={styles.container}>
+				<h1>Login</h1>
+			</div>
+		);
+	} else if (login === "register") {
+		return (
+			<div className={styles.container}>
+				<h1>Register</h1>
 			</div>
 		);
 	}
