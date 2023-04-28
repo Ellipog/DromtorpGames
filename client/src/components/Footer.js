e849fc91cc2b3e19dfc9b50b506066e2ff13e0a4
Ellipog <45790498+Ellipog@users.noreply.github.com>
2023-04-28 14:17:27 +0200

    Noe greier

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,4 @@
 import styles from "../css/Footer.module.css";
-import ChatIcon from "@mui/icons-material/Chat";
 import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import { Link } from "react-router-dom";
@@ -10,11 +9,8 @@ function Footer(props) {
 
 	return (
 		<div className={styles.footer}>
-			<Link to="/chat" className={activePage === "chat" ? styles.activePage : ""} onClick={() => setActivePage("chat")}>
-				<ChatIcon sx={{ fontSize: 50 }} />
-			</Link>
 			<Link to="/" className={activePage === "leaderboard" ? styles.activePage : ""} onClick={() => setActivePage("leaderboard")}>
-				<LeaderboardIcon sx={{ fontSize: 60 }} />
+				<LeaderboardIcon sx={{ fontSize: 50 }} />
 			</Link>
 			<Link to="/profile" className={activePage === "profile" ? styles.activePage : ""} onClick={() => setActivePage("profile")}>
 				<AccountBoxIcon sx={{ fontSize: 50 }} />
